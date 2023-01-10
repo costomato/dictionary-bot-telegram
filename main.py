ae5326173f3171509e35286d090a8f17cc2cff12
Kaustubh <sherlockaustubh@gmail.com>
2023-01-10 19:08:49 +0530

    fixed minor bugs

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ def inline_query(update, context):
 def error(update, context):
     # print(f'Update {update} caused error {context.error}')
     print('Update', update, 'caused error', context.error)
+    update.message.reply_text("Something bad happened.")
 
 if __name__ == '__main__':
     # updater = Updater(keys.bot_token, use_context=True)
@@ -103,7 +104,7 @@ if __name__ == '__main__':
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN, webhook_url=os.getenv('BASE_URL') + TOKEN)
 
     updater.idle()
